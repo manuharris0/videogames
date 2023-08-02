be7ced92949e73dc351905e0bbf70629173c7862
Manu <manuharris0@gmail.com>
2023-08-02 18:37:21 -0300

    genresService terminado

--- api/src/services/genresService.js
+++ api/src/services/genresService.js
@@ -1,40 +1,36 @@
 const axios = require('axios');
+
 const { Genre } = require('../db');
+
 require('dotenv').config();
 const { API_KEY } = process.env;
-const URL = `https://api.rawg.io/api/genres?key=70fb68c409b1498c9b31df5d9287bb59`;
 
 class genresService {
     
-    constructor() {
+    constructor() {};
 
+    async find() {
+        const genres = await Genre.findAll();
+        return genres;
     };
 
     async generate() {
         try {
-            const { data } = await axios.get(URL);
+
+            const { data } = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`);
             const genres = data.results.map(result => {
                 return {
                     name: result.name,
                     id: result.id
                 }
             })
-            const dbGenres = await Genre.bulkCreate(genres);
-            return dbGenres;
+            await Genre.bulkCreate(genres);
+            return this.find();
+
         } catch (error) {
             throw new Error(error.message)
         }
     }
-
-    async find() {
-        const genres = await Genre.findAll();
-        return genres;
-    }
-
-    create(data) {
-        // Acá voy a agregar un género a la base de datos
-    }
-
 };
 
 module.exports = genresService;
