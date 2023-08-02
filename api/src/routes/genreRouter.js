d1082fe4af6e91fd414edc380e481c098704713e
Manu <manuharris0@gmail.com>
2023-08-02 18:23:51 -0300

    ruteo de generos terminado

--- api/src/routes/genreRouter.js
+++ api/src/routes/genreRouter.js
@@ -1,10 +1,8 @@
 const { Router } = require('express');
 const getGenres = require('../handlers/genresHandlers/getGenres');
-const postGenre = require('../handlers/genresHandlers/postGenre');
 
 const genreRouter = Router();
 
 genreRouter.get('/', getGenres);
-// genreRouter.post('/', postGenre);
 
 module.exports = genreRouter;
