9abe9c4d573d72c5e4bb86f1c638d3c5d3858eb5
H4RR1S-D3V <harrisdm.dev@gmail.com>
2024-10-08 13:20:02 -0300

    Primera version publica

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'
 
-import { Landing }  from './Views/Landing/Landing';
-import { Home }  from './Views/Home/Home';
-import Detail from './Views/Detail/Detail';
-import Form  from './Views/Form/Form'
+import { Landing } from './views/Landing/Landing'
+import { Home } from './views/Home/Home'
+import Detail from './views/Detail/Detail'
+import Form from './views/Form/Form'
 function App() {
 
   return (
